64d6b5450033b580fbc2371fa17344146c7601ee
Mehedi Hasan <mmehedihasan514@gmail.com>
2025-10-09 00:49:18 -0400

    fix  test_data_divider error

--- test_data_divider.py
+++ test_data_divider.py
@@ -2,6 +2,6 @@ import logging
 from steps.data_divider import data_divider
 
 if __name__ == "__main__":
-    train_dir, val_dir = data_divider()()
+    train_dir, val_dir = data_divider()
     print("✅ Train directory:", train_dir)
     print("✅ Validation directory:", val_dir)
