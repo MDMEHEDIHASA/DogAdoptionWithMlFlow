f303ce57c3482bb245379ad4e393ea96b2252c24
Mehedi Hasan <mmehedihasan514@gmail.com>
2025-10-17 02:40:50 -0400

    add config file in run_training

--- run_training_pipeline.py
+++ run_training_pipeline.py
@@ -1,5 +1,5 @@
 import logging
-
+import yaml
 from steps.util import load_data
 from steps.train_model import train_model
 from steps.evaluate_model import evaluate_model
@@ -9,8 +9,13 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def run_training():
+
+    # Load configuration from YAML
+    with open("steps/config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
     training = train_pipeline(
-        load_data(),
+        load_data(config),
         train_model(),
         evaluate_model(),
     )
